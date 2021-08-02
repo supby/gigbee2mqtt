50bde309ce7afe309d42ef1d89c9d20db251db21
Andre Suzanovich <supius@gmail.com>
2021-08-02 21:21:59 +0200

    add test device to load in node table

--- geegbee2mqtt.go
+++ geegbee2mqtt.go
@@ -10,6 +10,10 @@ import (
 	"go.bug.st/serial.v1"
 )
 
+// Example of message:
+// MQTT publish: topic 'zigbee2mqtt/0x00124b000724ae04',
+// payload '{"cluster":"ssIasZone","data":{"batteryPercentageRemaining":200,"batteryVoltage":30,"zoneStatus":1},"device":{"friendlyName":"0x00124b000724ae04","ieeeAddr":"0x00124b000724ae04","model":"unknown","networkAddress":31211,"type":"Unknown"},"endpoint":{"ID":1,"_binds":[],"_configuredReportings":[],"clusters":{"genPowerCfg":{"attributes":{"batteryPercentageRemaining":200,"batteryVoltage":30}},"ssIasZone":{"attributes":{"zoneStatus":1}}},"deviceIeeeAddress":"0x00124b000724ae04","deviceNetworkAddress":31211,"inputClusters":[],"meta":{},"outputClusters":[]},"groupID":0,"linkquality":52,"meta":{"frameControl":{"direction":1,"disableDefaultResponse":true,"frameType":0,"manufacturerSpecific":false,"reservedBits":0},"manufacturerCode":null,"zclTransactionSequenceNumber":219},"type":"attributeReport"}'
+
 func main() {
 	mode := &serial.Mode{
 		BaudRate: 115200,
@@ -23,6 +27,18 @@ func main() {
 
 	/* Construct node table, cache of network nodes. */
 	t := zstack.NewNodeTable()
+	nodes := []zigbee.Node{
+		{
+			IEEEAddress:    0x00124b000724ae04,
+			NetworkAddress: 31211,
+			LogicalType:    0x02,
+			LQI:            0,
+			Depth:          0,
+			LastDiscovered: time.Now(),
+			LastReceived:   time.Now(),
+		},
+	}
+	t.Load(nodes)
 
 	/* Create a new ZStack struct. */
 	z := zstack.New(port, t)
