285bc2492714a6dc2be70e095ae1f9938906697f
Andre Suzanovich <supius@gmail.com>
2022-04-10 18:50:16 +0200

    move to ZB router to interfaces

--- router/interfaces.go
+++ router/interfaces.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"context"
+
 	"github.com/supby/gigbee2mqtt/mqtt"
 	"github.com/supby/gigbee2mqtt/types"
 )
@@ -8,4 +10,10 @@ import (
 type MQTTRouter interface {
 	ProccessMessageFromDevice(devMsg mqtt.DeviceMessage)
 	SubscribeOnSetMessage(callback func(devCmd types.DeviceCommandMessage))
+}
+
+type ZigbeeRouter interface {
+	SubscribeOnDeviceMessage(callback func(devMsg mqtt.DeviceMessage))
+	ProccessMessageToDevice(ctx context.Context, devCmd types.DeviceCommandMessage)
+	StartAsync(ctx context.Context)
 }
